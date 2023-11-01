883432a2a538144737a511d310d76c3630f92f35
aspsptyd <devadiroot@gmail.com>
2023-11-01 16:51:10 +0700

    Buat model sekaligus controller dengan php artisan make:model Reminder -r (otomatis create model dan controller)

---
+++ app/Http/Controllers/ReminderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reminder;
+use Illuminate\Http\Request;
+
+class ReminderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('reminder.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Reminder  $reminder
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reminder $reminder)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Reminder  $reminder
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Reminder $reminder)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Reminder  $reminder
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reminder $reminder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Reminder  $reminder
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reminder $reminder)
+    {
+        //
+    }
+}
