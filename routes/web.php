83d62e2236d6b3ddbe4b8cb6d15719a6f593fa57
aspsptyd <devadiroot@gmail.com>
2023-11-02 18:02:24 +0700

    Routing dashboard dan reminder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ReminderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::controller(DashboardController::class)->prefix('/')->group(function () {
+    Route::get('/', 'index')->name('dashboard');
+});
 
 Route::controller(BarangController::class)->prefix('barang')->group(function () {
     Route::get('', 'index')->name('barang');
@@ -28,4 +30,18 @@ Route::controller(BarangController::class)->prefix('barang')->group(function () 
 
     Route::get('delete/{id}', 'delete')->name('barang.delete');
     Route::post('detail/{id}', 'detail')->name('barang.detail');
+});
+
+Route::controller(ReminderController::class)->prefix('reminder')->group(function () {
+    Route::get('', 'index')->name('reminder');
+    Route::get('create', 'create')->name('reminder.create');
+    Route::get('edit/{id}', 'edit')->name('reminder.edit');
+
+    Route::post('store', 'store')->name('reminder.store');
+    Route::post('update/{id}', 'update')->name('reminder.update');
+
+    Route::get('delete/{id}', 'delete')->name('reminder.delete');
+    Route::get('close/{id}', 'close')->name('reminder.close');
+    Route::get('open/{id}', 'open')->name('reminder.open');
+    Route::post('detail/{id}', 'detail')->name('reminder.detail');
 });
