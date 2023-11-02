fffc88834f35c0151ca529fcb82fc45f50f28c41
aspsptyd <devadiroot@gmail.com>
2023-11-02 18:01:50 +0700

    Sesuaikan fitur dan tambah beberapa menu yang akan di develop berikutnya

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,11 +1,8 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+    <a class="sidebar-brand d-flex" href="{{ route('dashboard') }}" style="margin-left: -10px">
+        <div class="sidebar-brand-text mx-3">recomposejob</div>
     </a>
 
     <!-- Divider -->
@@ -23,120 +20,62 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Priorities
+        My Priorities
     </div>
 
     <li class="nav-item">
-        <a class="nav-link" href="{{ asset('sbadmin') }}/tables.html">
+        <a class="nav-link" href="{{ route('reminder') }}">
             <i class="fas fa-fw fa-bell"></i>
-            <span>Reminder</span></a>
+            <span>Reminder Task</span></a>
     </li>
 
-    <li class="nav-item">
+    {{-- <li class="nav-item" style="margin-top: -15px">
         <a class="nav-link" href="{{ route('barang') }}">
             <i class="fas fa-fw fa-box"></i>
             <span>Data Barang</span></a>
-    </li>
+    </li> --}}
 
-    <!-- Divider -->
     <hr class="sidebar-divider">
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Interface
+       Good & Proper Test
     </div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-            aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="{{ asset('sbadmin') }}/buttons.html">Buttons</a>
-                <a class="collapse-item" href="{{ asset('sbadmin') }}/cards.html">Cards</a>
-            </div>
-        </div>
+        <a class="nav-link" href="#">
+            <i class="fas fa-fw fa-database"></i>
+            <span>Master Testing<span class="badge badge-success ml-2">Soon</span></span></a>
     </li>
 
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-            aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilities</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-            data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="{{ asset('sbadmin') }}/utilities-color.html">Colors</a>
-                <a class="collapse-item" href="{{ asset('sbadmin') }}/utilities-border.html">Borders</a>
-                <a class="collapse-item" href="{{ asset('sbadmin') }}/utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="{{ asset('sbadmin') }}/utilities-other.html">Other</a>
-            </div>
-        </div>
+    <li class="nav-item" style="margin-top: -15px">
+        <a class="nav-link" href="#">
+            <i class="fas fa-fw fa-flask"></i>
+            <span>Testing & Validasi<span class="badge badge-success ml-2">Soon</span></span></span></a>
     </li>
 
-    <!-- Divider -->
+    <li class="nav-item" style="margin-top: -15px">
+        <a class="nav-link" href="#">
+            <i class="fas fa-fw fa-bug"></i>
+            <span>Bug & Issue<span class="badge badge-success ml-2">Soon</span></span></span></a>
+    </li>
+
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
     <div class="sidebar-heading">
-        Addons
-    </div>
+        Settings
+     </div>
+ 
+     <li class="nav-item">
+         <a class="nav-link" href="#">
+             <i class="fas fa-fw fa-database"></i>
+             <span>Settings</span></a>
+     </li>
 
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-            aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="{{ asset('sbadmin') }}/login.html">Login</a>
-                <a class="collapse-item" href="{{ asset('sbadmin') }}/register.html">Register</a>
-                <a class="collapse-item" href="{{ asset('sbadmin') }}/forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="{{ asset('sbadmin') }}/404.html">404 Page</a>
-                <a class="collapse-item" href="{{ asset('sbadmin') }}/blank.html">Blank Page</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="{{ asset('sbadmin') }}/charts.html">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
-    </li>
-
-    <!-- Nav Item - Tables -->
-    <li class="nav-item">
-        <a class="nav-link" href="{{ asset('sbadmin') }}/tables.html">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span></a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
-
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
-
-    <!-- Sidebar Message -->
-    <div class="sidebar-card d-none d-lg-flex">
-        <img class="sidebar-card-illustration mb-2" src="{{ asset('sbadmin') }}/img/undraw_rocket.svg" alt="...">
-        <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-        <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-    </div>
+     <li class="nav-item" style="margin-top: -15px">
+         <a class="nav-link" href="#">
+             <i class="fas fa-fw fa-bug"></i>
+             <span>Logout</span></a>
+     </li>
 
 </ul>
