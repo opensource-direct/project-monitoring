3222e16965f233cb6de67236fb29aeb8ff03ae21
septiyadi <septiyadi@mhs.pelitabangsa.ac.id>
2023-11-13 14:49:03 +0700

    Tambahkan placeholder untuk form reminder

--- resources/views/reminder/form.blade.php
+++ resources/views/reminder/form.blade.php
@@ -17,7 +17,7 @@
                         <div class="input-group-prepend">
                             <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M168.2 384.9c-15-5.4-31.7-3.1-44.6 6.4c-8.2 6-22.3 14.8-39.4 22.7c5.6-14.7 9.9-31.3 11.3-49.4c1-12.9-3.3-25.7-11.8-35.5C60.4 302.8 48 272 48 240c0-79.5 83.3-160 208-160s208 80.5 208 160s-83.3 160-208 160c-31.6 0-61.3-5.5-87.8-15.1zM26.3 423.8c-1.6 2.7-3.3 5.4-5.1 8.1l-.3 .5c-1.6 2.3-3.2 4.6-4.8 6.9c-3.5 4.7-7.3 9.3-11.3 13.5c-4.6 4.6-5.9 11.4-3.4 17.4c2.5 6 8.3 9.9 14.8 9.9c5.1 0 10.2-.3 15.3-.8l.7-.1c4.4-.5 8.8-1.1 13.2-1.9c.8-.1 1.6-.3 2.4-.5c17.8-3.5 34.9-9.5 50.1-16.1c22.9-10 42.4-21.9 54.3-30.6c31.8 11.5 67 17.9 104.1 17.9c141.4 0 256-93.1 256-208S397.4 32 256 32S0 125.1 0 240c0 45.1 17.7 86.8 47.7 120.9c-1.9 24.5-11.4 46.3-21.4 62.9zM144 272a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm144-32a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm80 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg></span>
                         </div>
-                        <input type="text" class="form-control" id="reminder_title" name="reminder_title" value="{{ isset($reminder) ? $reminder->reminder_title : '' }}">
+                        <input type="text" class="form-control" id="reminder_title" name="reminder_title" value="{{ isset($reminder) ? $reminder->reminder_title : '' }}" placeholder="{{ isset($reminder) ? $reminder->reminder_title : 'Inputkan title reminder' }}">
                     </div>
                     <label for="reminder_detail">Reminder Details</label>
                     <div class="form-group input-group">
@@ -26,7 +26,7 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#3a4d4d}</style><path d="M64 464c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16H224v80c0 17.7 14.3 32 32 32h80V448c0 8.8-7.2 16-16 16H64zM64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V154.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0H64zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120z"/></svg>
                             </span>
                         </div>
-                        <textarea type="text" class="form-control" id="reminder_detail" name="reminder_detail" style="height: 200px">{{ isset($reminder) ? $reminder->reminder_detail : '' }}</textarea>
+                        <textarea type="text" class="form-control" id="reminder_detail" name="reminder_detail" style="height: 200px" placeholder="{{ isset($reminder) ? $reminder->reminder_title : 'Isikan juga detail reminder' }}">{{ isset($reminder) ? $reminder->reminder_detail : '' }}</textarea>
                     </div>
                     <label for="created_by">Created by</label>
                     <div class="form-group input-group">
@@ -44,7 +44,7 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#334b4b}</style><path d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376V479.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"/></svg>    
                             </span>
                         </div>
-                        <input type="text" class="form-control" id="project_name" name="project_name" value="{{ isset($reminder) ? $reminder->project_name : '' }}">
+                        <input type="text" class="form-control" id="project_name" name="project_name" value="{{ isset($reminder) ? $reminder->project_name : '' }}"  placeholder="{{ isset($reminder) ? $reminder->reminder_title : 'Inputkan nama project terkait' }}">
                     </div>
                     <label for="status_notes">Pilih Status</label>
                     <div class="form-group input-group">
